0c4405d282865a98e5d213eb07cd1db6ad68b829
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-21 12:24:50 -0400

    added getBookmarks method and tests passed

--- scripts/api.js
+++ scripts/api.js
@@ -1,7 +1,16 @@
 'use strict';
+/* global $ */
 
 const api = (function() {
 
-  const BASE_URL = 'https://thinkful-list-api.herokuapp.com';
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/mitchg';
+
+  const getBookmarks = function(callback) {
+    return $.getJSON(`${BASE_URL}/bookmarks`, callback);
+  };
+
+  return {
+    getBookmarks,
+  };
 
 }());
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,16 @@
 'use strict';
-/* global bookmarkList, $ */
+/* global bookmarkList, api, $ */
 
 $(function () {
   bookmarkList.bindEventListeners();
 });
+
+// $.getJSON('https://thinkful-list-api.herokuapp.com/mitchg/bookmarks'), (response) => {
+//   console.log('api response:', response);
+// };
+
+api.getBookmarks(function(data){
+  console.log(data);
+});
+
+console.log(api.BASE_URL);
